b466501dff4fb8313f6c018c01f2c334eff51729
jaheer <khanjaheerhayat@gmail.com>
2026-08-19 13:46:35 +0530

    django authentiction project

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators  import login_required
 
 from testapp.forms import signupForm
-
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -21,4 +21,11 @@ def logout_view(request):
     return render(request,'testapp/logout.html')
 def signup_view(request):
     form=signupForm()
+    if request.method =='POST':
+        form=signupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+            return HttpResponseRedirect("/accounts/login/")
     return render(request,'testapp/signup.html',{'form':form})
